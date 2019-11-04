abe78daad03a0268a73cf146d2d1449dbc6d1d2f
Jeffrey Omidvaran <jeffomidvaran@gmail.com>
2019-11-04 15:32:29 -0800

    improved arpegio

--- projectCodeFiles/open_and_ interpret_data.py
+++ projectCodeFiles/open_and_ interpret_data.py
@@ -8,120 +8,112 @@ import itertools
 import midi2audio
 
 
-nC2 =  0
-nC1 =  12
-C0 =  24
-C1 =  36
-C2 =  48
-C3 =  60
-C4 =  72
-
-notes = {
-    "c2n" , 0,
-    "cs2n", 1,
-    "d2n" , 2,
-    "ds2n", 3,
-    "e2n" , 4,
-    "f2n" , 5,
-    "fs2n", 6,
-    "g2n" , 7,
-    "gs2n", 8,
-    "a2n" , 9,
-    "as2n", 10,
-    "b2n" , 11,
+note = {
+    "c2n" : 0,
+    "cs2n": 1,
+    "d2n" : 2,
+    "ds2n": 3,
+    "e2n" : 4,
+    "f2n" : 5,
+    "fs2n": 6,
+    "g2n" : 7,
+    "gs2n": 8,
+    "a2n" : 9,
+    "as2n": 10,
+    "b2n" : 11,
     
     
-    "c1n" , 12,
-    "cs1n", 13,
-    "d1n" , 14,
-    "ds1n", 15,
-    "e1n" , 16,
-    "f1n" , 17,
-    "fs1n", 18,
-    "g1n" , 19,
-    "gs1n", 20,
-    "a1n" , 21,
-    "as1n", 22,
-    "b1n" , 23,
+    "c1n" : 12,
+    "cs1n": 13,
+    "d1n" : 14,
+    "ds1n": 15,
+    "e1n" : 16,
+    "f1n" : 17,
+    "fs1n": 18,
+    "g1n" : 19,
+    "gs1n": 20,
+    "a1n" : 21,
+    "as1n": 22,
+    "b1n" : 23,
     
      
-    "c0" , 24,
-    "cs0", 25,
-    "d0" , 26,
-    "ds0", 27,
-    "e0" , 28,
-    "f0" , 29,
-    "fs0", 30,
-    "g0" , 31,
-    "gs0", 32,
-    "a0" , 33,
-    "as0", 34,
-    "b0" , 35,
+    "c0" :24,
+    "cs0":25,
+    "d0" :26,
+    "ds0":27,
+    "e0" :28,
+    "f0" :29,
+    "fs0":30,
+    "g0" :31,
+    "gs0":32,
+    "a0" :33,
+    "as0":34,
+    "b0" :35,
       
-    "c1" , 36,
-    "cs1", 37,
-    "d1" , 38,
-    "ds1", 39,
-    "e1" , 40,
-    "f1" , 41,
-    "fs1", 42,
-    "g1" , 43,
-    "gs1", 44,
-    "a1" , 45,
-    "as1", 46,
-    "b1" , 47,
+    "c1" :36,
+    "cs1":37,
+    "d1" :38,
+    "ds1":39,
+    "e1" :40,
+    "f1" :41,
+    "fs1":42,
+    "g1" :43,
+    "gs1":44,
+    "a1" :45,
+    "as1":46,
+    "b1" :47,
      
-    "c2" , 48,
-    "cs2", 49,
-    "d2" , 50,
-    "ds2", 51,
-    "e2" , 52,
-    "f2" , 53,
-    "fs2", 54,
-    "g2" , 55,
-    "gs2", 56,
-    "a2" , 57,
-    "as2", 58,
-    "b2" , 59,
+    "c2" :48,
+    "cs2":49,
+    "d2" :50,
+    "ds2":51,
+    "e2" :52,
+    "f2" :53,
+    "fs2":54,
+    "g2" :55,
+    "gs2":56,
+    "a2" :57,
+    "as2":58,
+    "b2" :59,
 
     
-    "c3" , 60,
-    "cs3", 61,
-    "d3" , 62,
-    "ds3", 63,
-    "e3" , 64,
-    "f3" , 65,
-    "fs3", 66,
-    "g3" , 67,
-    "gs3", 68,
-    "a3" , 69,
-    "as3", 70,
-    "b3" , 71,
+    "c3" :60,
+    "cs3":61,
+    "d3" :62,
+    "ds3":63,
+    "e3" :64,
+    "f3" :65,
+    "fs3":66,
+    "g3" :67,
+    "gs3":68,
+    "a3" :69,
+    "as3":70,
+    "b3" :71,
      
-    "c4" , 72,
-    "cs4", 73,
-    "d4" , 74,
-    "ds4", 75,
-    "e4" , 76,
-    "f4" , 77,
-    "fs4", 78,
-    "g4" , 79,
-    "gs4", 80,
-    "a4" , 81,
-    "as4", 82,
+    "c4" :72,
+    "cs4":73,
+    "d4" :74,
+    "ds4":75,
+    "e4" :76,
+    "f4" :77,
+    "fs4":78,
+    "g4" :79,
+    "gs4":80,
+    "a4" :81,
+    "as4":82,
       
-    "c5" , 84,
-    "cs5", 85,
-    "d5" , 86,
-    "ds5", 87,
-    "e5" , 88,
-    "f5" , 89,
-    "fs5", 90,
-    "g5" , 91,
-    "gs5", 92,
-    "a5" , 93,
-    "as5", 94,
-    "b5" , 95,
+    "c5" :84,
+    "cs5":85,
+    "d5" :86,
+    "ds5":87,
+    "e5" :88,
+    "f5" :89,
+    "fs5":90,
+    "g5" :91,
+    "gs5":92,
+    "a5" :93,
+    "as5":94,
+    "b5" :95,
     
     }
 
@@ -281,29 +273,38 @@ def createMelody(melody_pitches, melody_rhythm, clean_data, alert_dat, midiObj):
 
 
 
-def create_midi_with_melody(clean_data, alert_data, name, rhythm = False):
+def create_midi_with_melody(clean_data, alert_data, name, rhythm):
     midiObj = MIDIFile(3)  # create one track
     midiObj.addTempo(0, 0, 150)
 
     ####################################################################
-    ##################  CREATE RHYTHM ##################################  
+    ##################  CREATE ARPEGGIATION  ########################### 
     ####################################################################
 
-    if( rhythm == True):    
+    if(rhythm == True):    
         clean_rhythm_length = round(len(clean_data)/16)
-        note_cutoff = 0.05
+        note_cutoff = 0.02
 
         for i in range(clean_rhythm_length):
             forward = i * 4
-            ######## HIGH TONIC NOTE #################### 
             #               track, channel, pitch, time      , duration    , volume
-            midiObj.addNote(0    , 0      , 48   , forward  , WHOLE_NOTE - note_cutoff, 100)
+            midiObj.addNote(0, 0, note["c1"], forward                   , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE    , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(2, 0, note["c3"], forward + SIXTEEN_NOTE *2 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *3 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(0, 0, note["c1"], forward + SIXTEEN_NOTE *4 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *5 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(2, 0, note["c3"], forward + SIXTEEN_NOTE *6 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *7 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(0, 0, note["c1"], forward + SIXTEEN_NOTE *8 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *9 , SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(2, 0, note["c3"], forward + SIXTEEN_NOTE *10, SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *11, SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(0, 0, note["c1"], forward + SIXTEEN_NOTE *12, SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *13, SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(2, 0, note["c3"], forward + SIXTEEN_NOTE *14, SIXTEEN_NOTE - note_cutoff, 100)
+            midiObj.addNote(1, 0, note["g2"], forward + SIXTEEN_NOTE *15, SIXTEEN_NOTE - note_cutoff, 100)
 
-            ######## DOMINANT NOTE ###################### 
-            midiObj.addNote(1, 0, 55, forward, WHOLE_NOTE - note_cutoff, 100)
-
-            #######  LOW TONIC NOTE #####################
-            midiObj.addNote(2, 0, 36, forward, WHOLE_NOTE - note_cutoff, 100)
 
     ####################################################################
     ##################  CREATE DRONE ###################################  
@@ -378,8 +379,13 @@ if __name__ == "__main__":
     createDirectory("midiFiles")
     create_midi_with_melody(clean_data, 
                             alert_data,
-                            "clean_rhythm_alert_melody", 
+                            "clean_drone_alert_melody", 
                             False) 
+
+    create_midi_with_melody(clean_data, 
+                            alert_data,
+                            "clean_rhythm_alert_melody", 
+                            True) 
 
     # CreateDroneMidiFile(clean_data, "CleanDrone")
     # createMidiFile(clean_data, "clean_arpeg", True)
